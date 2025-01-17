9a79d683478d49cc043309a6a51a48316d862c11
Bankole <techybanky@gmail.com>
2025-01-17 21:24:42 +0100

    ci(workflows): fix added commit message to release-it ci command

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'type-empty': process.env.CI ? [0, 'always'] : [2, 'never'],
+    'subject-empty': process.env.CI ? [0, 'always'] : [2, 'never'],
     'type-enum': [
       2, // Level: 2 = error
       'always', // Condition: enforce this rule always
