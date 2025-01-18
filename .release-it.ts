0268e2295e23469e2b9e8171a46e7209457dfa94
Bankole <techybanky@gmail.com>
2025-01-18 01:34:26 +0100

    wip(workflows): replacing conventional-changelog with standard-version

--- .release-it.ts
+++ .release-it.ts
@@ -8,6 +8,7 @@ export default {
     requireUpstream: true,
     commitMessage: 'chore(release): v${version}',
     tagName: 'v${version}',
+    changelog: "npm run release"
   },
   github: {
     release: true,
@@ -16,75 +17,75 @@ export default {
   npm: {
     publish: false,
   },
-  plugins: {
-    '@release-it/conventional-changelog': {
-      infile: "CHANGELOG.md",
-      // "writerOpts": {
-      //   "groupBy": "type"
-      // },
-      preset: {
-        name: "conventionalcommits",
-        // types: [
-        //   {
-        //     "type": "feat",
-        //     "section": "✨Features"
-        //   },
-        //   {
-        //     "type": "fix",
-        //     "section": "🐛 Bug Fixes"
-        //   },
-        //   {
-        //     "type": "chore",
-        //     "hidden": false,
-        //     "section": "🚚 Chores"
-        //   },
-        //   {
-        //     "type": "docs",
-        //     "hidden": false,
-        //     "section": "📝 Documentation"
-        //   },
-        //   {
-        //     "type": "style",
-        //     "hidden": false,
-        //     "section": "🎨 Styling"
-        //   },
-        //   {
-        //     "type": "refactor",
-        //     "hidden": false,
-        //     "section": "♻️ Code Refactoring"
-        //   },
-        //   {
-        //     "type": "perf",
-        //     "hidden": false,
-        //     "section": "⚡️ Performance Improvements"
-        //   },
-        //   {
-        //     "type": "test",
-        //     "hidden": false,
-        //     "section": "✅ Testing"
-        //   },
-        //   {
-        //     "type": "build",
-        //     "hidden": false,
-        //     "section": "🚧 Build"
-        //   },
-        //   {
-        //     "type": "wip",
-        //     "hidden": false,
-        //     "section": "⚒️ Work In Progress"
-        //   },
-        //   {
-        //     "type": "revert",
-        //     "hidden": false,
-        //     "section": "🔁 Revert"
-        //   },
-        //   {
-        //     "type": "ci",
-        //     "hidden": false,
-        //     "section": "👷‍♂️ Continuous Integration"
-        //   }
-        // ]
-      }
-    }
-  },
+  // plugins: {
+  //   '@release-it/conventional-changelog': {
+  //     infile: "CHANGELOG.md",
+  //     // "writerOpts": {
+  //     //   "groupBy": "type"
+  //     // },
+  //     preset: {
+  //       name: "conventionalcommits",
+  //       // types: [
+  //       //   {
+  //       //     "type": "feat",
+  //       //     "section": "✨Features"
+  //       //   },
+  //       //   {
+  //       //     "type": "fix",
+  //       //     "section": "🐛 Bug Fixes"
+  //       //   },
+  //       //   {
+  //       //     "type": "chore",
+  //       //     "hidden": false,
+  //       //     "section": "🚚 Chores"
+  //       //   },
+  //       //   {
+  //       //     "type": "docs",
+  //       //     "hidden": false,
+  //       //     "section": "📝 Documentation"
+  //       //   },
+  //       //   {
+  //       //     "type": "style",
+  //       //     "hidden": false,
+  //       //     "section": "🎨 Styling"
+  //       //   },
+  //       //   {
+  //       //     "type": "refactor",
+  //       //     "hidden": false,
+  //       //     "section": "♻️ Code Refactoring"
+  //       //   },
+  //       //   {
+  //       //     "type": "perf",
+  //       //     "hidden": false,
+  //       //     "section": "⚡️ Performance Improvements"
+  //       //   },
+  //       //   {
+  //       //     "type": "test",
+  //       //     "hidden": false,
+  //       //     "section": "✅ Testing"
+  //       //   },
+  //       //   {
+  //       //     "type": "build",
+  //       //     "hidden": false,
+  //       //     "section": "🚧 Build"
+  //       //   },
+  //       //   {
+  //       //     "type": "wip",
+  //       //     "hidden": false,
+  //       //     "section": "⚒️ Work In Progress"
+  //       //   },
+  //       //   {
+  //       //     "type": "revert",
+  //       //     "hidden": false,
+  //       //     "section": "🔁 Revert"
+  //       //   },
+  //       //   {
+  //       //     "type": "ci",
+  //       //     "hidden": false,
+  //       //     "section": "👷‍♂️ Continuous Integration"
+  //       //   }
+  //       // ]
+  //     }
+  //   }
+  // },
 } satisfies Config;
