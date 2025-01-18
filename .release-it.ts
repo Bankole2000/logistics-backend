35dc81241b8a28081313da6dda50013ca5a83998
Bankole <techybanky@gmail.com>
2025-01-18 09:02:44 +0100

    fix(workflows): fixing release workflow

--- .release-it.ts
+++ .release-it.ts
@@ -17,10 +17,9 @@ export default {
   npm: {
     publish: false,
   },
-  // hooks: {
-    
-  //   'before:version:release': 'echo \'Skipping npm version bump...\''
-  // }
+  hooks: {
+    'before:version:release': 'echo \'Skipping npm version bump...\''
+  }
   // plugins: {
   //   '@release-it/conventional-changelog': {
   //     infile: "CHANGELOG.md",
